4af54f64cdc0e3d6dd33a96d3402c04b23f956cb
rishikeshct <rishikeshctonne@gmail.com>
2017-06-19 11:18:40 +0530

    WebSocket commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var socket =  require('socket.io');
+
+var server = app.listen(3000,function(){
+    console.log('listening to the 3000 port');
+});
+
+app.use(express.static('public'));
+
+
+//socket setup
+var io = socket(server); //socket.io to work on this server
+
+io.on('connection',function(socket){   //socket will be diff for every client
+  console.log('made socket connection '+socket.id);
+
+    socket.on('chat' , function(data){
+        console.log(data);
+        io.sockets.emit('chat', data);
+      });
+
+      // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
---
+++ public/chat.js
@@ -0,0 +1,35 @@
+//make connection
+
+var socket = io.connect('http://localhost:3000');
+
+
+var message = document.getElementById('message');
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output');
+      feedback = document.getElementById('feedback');
+
+//Event emit when someone clicks button
+
+btn.addEventListener('click',function(){
+
+  socket.emit('chat', {
+    msg : message.value,
+    handle: handle.value
+
+  });
+
+});
+
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+})
+
+socket.on('chat', function(data){
+     feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.msg + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
